3ba4410883abdefdf854892a67c4070276bc26d8
FrankMartoccia <92721828+FrankMartoccia@users.noreply.github.com>
2022-07-05 18:03:19 +0200

    Add Signed_Parallel_Multiplier and comments

---
+++ src/Signed_Parallel_Multiplier.vhd
@@ -0,0 +1,56 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Signed_Parallel_Multiplier is 
+    generic (
+        Nbit_x: positive; -- Number of bits of the vector x_p
+        Nbit_w: positive  -- Number of bits of the vector w_p
+    );
+    port(
+        x_p_sign: in std_logic_vector(Nbit_x - 1 downto 0);
+        w_p_sign: in std_logic_vector(Nbit_w - 1 downto 0);
+        p_sign  : out std_logic_vector(Nbit_x + Nbit_w - 1 downto 0) -- Product vector with sign
+    );
+end entity Signed_Parallel_Multiplier;
+
+architecture beh of Signed_Parallel_Multiplier is 
+
+	component Unsigned_Parallel_Multiplier
+		generic (
+			Nbit_x: positive;
+			Nbit_w: positive
+		);
+		port(
+			x_p: in std_logic_vector(Nbit_x - 1 downto 0);
+			w_p: in std_logic_vector(Nbit_w - 1 downto 0);
+			p  : out std_logic_vector(Nbit_x + Nbit_w - 1 downto 0) -- Product vector without sign
+			);
+	end component Unsigned_Parallel_Multiplier;
+
+	signal x_sign: std_logic; -- Sign of the x_p_sign vector
+    signal w_sign: std_logic; -- Sign of the w_p_sign vector
+	signal x_p_unsign: std_logic_vector(Nbit_x - 1 downto 0); -- x_p vector without sign
+    signal w_p_unsign: std_logic_vector(Nbit_w - 1 downto 0); -- w_p vector without sign
+    signal p_unsign  : std_logic_vector(Nbit_x + Nbit_w - 1 downto 0); -- p vector without sign
+	
+begin
+	
+	x_sign <= x_p_sign(Nbit_x - 1); -- Value of the MSB used for the sign representation of the x_p_sign vector
+    w_sign <= w_p_sign(Nbit_w - 1); -- Value of the MSB used for the sign representation of the w_p_sign vector
+	x_p_unsign <= std_logic_vector(abs(signed(x_p_sign))); -- Absolute value of the signal to pass to the Unsigned_Parallel_Multiplier
+	w_p_unsign <= std_logic_vector(abs(signed(w_p_sign))); -- Absolute value of the signal to pass to the Unsigned_Parallel_Multiplier
+	p_sign <= std_logic_vector(unsigned(not(p_unsign)) + 1) when ((x_sign xor w_sign) = '1') else p_unsign;
+	
+	UPM: Unsigned_Parallel_Multiplier
+		generic map(
+            Nbit_x => Nbit_x,
+            Nbit_w => Nbit_w
+        )
+        port map(
+            x_p =>  x_p_unsign,
+            w_p =>  w_p_unsign,
+            p   => p_unsign
+        );
+	
+end architecture beh;
--- src/Unsigned_Parallel_Multiplier.vhd
+++ src/Unsigned_Parallel_Multiplier.vhd
@@ -3,17 +3,17 @@ use IEEE.std_logic_1164.all;
 
 entity Unsigned_Parallel_Multiplier is
     generic (
-        Nbit_x : positive; 
-        Nbit_w: positive
+        Nbit_x: positive; -- Number of bits of the vector x_p
+        Nbit_w: positive   -- Number of bits of the vector w_p
     );
     port(
-        x_p: in std_logic_vector(Nbit_x - 1 downto 0);
+        x_p: in std_logic_vector(Nbit_x - 1 downto 0); 
         w_p: in std_logic_vector(Nbit_w - 1 downto 0);
-        p: out std_logic_vector(Nbit_x + Nbit_w - 1 downto 0)
+        p  : out std_logic_vector(Nbit_x + Nbit_w - 1 downto 0) -- Product vector
     );
 end entity Unsigned_Parallel_Multiplier;
 
-architecture rtl of Unsigned_Parallel_Multiplier is
+architecture beh of Unsigned_Parallel_Multiplier is
     component FULL_ADDER is
     port
     (
@@ -35,12 +35,12 @@ architecture rtl of Unsigned_Parallel_Multiplier is
     );
     end component;
 	
-	signal carry_signal: std_logic_vector((Nbit_x)*(Nbit_w-1) - 1 downto 0);
-	signal sum_signal: std_logic_vector((Nbit_x - 1)*(Nbit_w - 2) - 1 downto 0);
-	signal product: std_logic_vector((Nbit_x*Nbit_w)-1 downto 0);
+	signal carry_signal: std_logic_vector((Nbit_x)*(Nbit_w-1) - 1 downto 0); -- Vector that mantains the values of the carry
+	signal sum_signal: std_logic_vector((Nbit_x - 1)*(Nbit_w - 2) - 1 downto 0); -- Vector that mantains the values of the sums
+	signal product: std_logic_vector((Nbit_x*Nbit_w)-1 downto 0); -- Vector that mantains the values of the partial products
 	
 begin
-	
+	-- Process that computes the values of the partial products and initializes the values of the product vector
 	p_process : process(x_p,w_p)
 	begin
 		for i in 0 to Nbit_w-1 loop
@@ -50,7 +50,9 @@ begin
 		end loop;
 	end process p_process;
 	
-	p(0) <= product(0);
+	p(0) <= product(0); -- Setting of the first bit of the result
+	
+	-- Generation of the Unsigned Parallel Multiplier's components 
 	GEN_row: for i in 1 to Nbit_w-1 generate
 		GEN_column: for j in 0 to Nbit_x-1 generate
 		FIRST_ROW: 		if i = 1 generate
@@ -60,7 +62,7 @@ begin
 								(
 									a => product(1),
 									b => product(Nbit_x),
-									s => p(1),
+									s => p(1), -- Setting of the second bit of the result
 									cout => carry_signal(j)
 								);
 			end generate RIGHT_HA;
@@ -75,17 +77,16 @@ begin
 									cout => carry_signal(j)
 								);
 			end generate CENTRAL_FA;
-			LEFT_FA:			if j = Nbit_x-1 generate
-								FIRST_ROW: FULL_ADDER
+			LEFT_HA:			if j = Nbit_x-1 generate
+								FIRST_ROW: HALF_ADDER
 								port map
 								(
-									a => '0',
+									a => carry_signal(j-1),
 									b => product(Nbit_x+j),
-									cin => carry_signal(j-1),
 									s => sum_signal(j-1),
 									cout => carry_signal(j)
 								);
-			end generate LEFT_FA;
+			end generate LEFT_HA;
 		end generate FIRST_ROW;
 		
 		INTERNAL_ROW:	if i > 1 and i < Nbit_w-1 generate
@@ -95,7 +96,7 @@ begin
 								(
 									a => sum_signal((Nbit_x-1)*(i-2)),
 									b => product(Nbit_x*i),
-									s => p(i),
+									s => p(i), -- Setting of the i-bit of the result
 									cout => carry_signal(Nbit_x*(i-1))
 								);
 			end generate RIGHT_HA;
@@ -141,7 +142,7 @@ begin
 								a => sum_signal((Nbit_x-1)*(i-2) + j),
 								b => product((Nbit_x*i) + j),
 								cin => carry_signal(Nbit_x*(i-1) + j-1),
-								s => p(i+j),
+								s => p(i+j), -- Setting of the internal bits of the result
 								cout => carry_signal((Nbit_x*(i-1)) + j)
 								);
 			end generate CENTRAL_FA;
@@ -152,11 +153,11 @@ begin
 								a => carry_signal((Nbit_x)*(i-1) - 1),
 								b => product((Nbit_x*i) + j),
 								cin => carry_signal(Nbit_x*(i-1) + j-1),
-								s => p(i+j),
+								s => p(i+j), -- Setting of the last bit of the result
 								cout => p(i+j+1)
 							);
 			end generate LEFT_FA;
 		end generate LAST_ROW;
 		end generate GEN_column;
 	end generate GEN_row;
-end architecture rtl;
+end architecture beh;
