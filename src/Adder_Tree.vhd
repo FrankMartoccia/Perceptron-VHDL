5f8fdb4ff7afc34cd4be9f9114b62872b5c4f115
FrankMartoccia <92721828+FrankMartoccia@users.noreply.github.com>
2022-07-06 18:42:35 +0200

    Errors fixed

--- src/Adder_Tree.vhd
+++ src/Adder_Tree.vhd
@@ -6,16 +6,16 @@ entity Adder_Tree is
     port(
         
         -- Values of the products that have to be summed
-        l1_s1_in1 : out std_logic_vector(16 downto 0);
-        l1_s1_in2 : out std_logic_vector(16 downto 0);
-        l1_s2_in1 : out std_logic_vector(16 downto 0);
-        l1_s2_in2 : out std_logic_vector(16 downto 0);
-        l1_s3_in1 : out std_logic_vector(16 downto 0);
-        l1_s3_in2 : out std_logic_vector(16 downto 0);
-        l1_s4_in1 : out std_logic_vector(16 downto 0);
-        l1_s4_in2 : out std_logic_vector(16 downto 0);
-        l1_s5_in1 : out std_logic_vector(16 downto 0);
-        l1_s5_in2 : out std_logic_vector(16 downto 0);
+        l1_s1_in1 : in std_logic_vector(16 downto 0);
+        l1_s1_in2 : in std_logic_vector(16 downto 0);
+        l1_s2_in1 : in std_logic_vector(16 downto 0);
+        l1_s2_in2 : in std_logic_vector(16 downto 0);
+        l1_s3_in1 : in std_logic_vector(16 downto 0);
+        l1_s3_in2 : in std_logic_vector(16 downto 0);
+        l1_s4_in1 : in std_logic_vector(16 downto 0);
+        l1_s4_in2 : in std_logic_vector(16 downto 0);
+        l1_s5_in1 : in std_logic_vector(16 downto 0);
+        l1_s5_in2 : in std_logic_vector(16 downto 0);
 
         clk: in std_logic;
         rst: in std_logic;
@@ -89,13 +89,13 @@ begin
     -- First layer of adders
     l1_s1: Ripple_carry_adder 
         generic map(
-            Nbit => 18;
+            Nbit => 18
         )
         port map(
-            a => l1_s1_in1;
-            b => l1_s1_in2;
-            cin => '0';
-            s => l1_s1_out;
+            a => l1_s1_in1,
+            b => l1_s1_in2,
+            cin => '0',
+            s => l1_s1_out,
             cout => open
         );
     l1_r1: DFF_N
@@ -103,21 +103,21 @@ begin
             N => 18
         )
         port map(
-            clk => clk;
-            a_rst_n => rst;
-            d => l1_s1_out;
-            q => l2_s1_in1;
+            clk => clk,
+            a_rst_n => rst,
+            d => l1_s1_out,
+            q => l2_s1_in1
         );
 
     l1_s2: Ripple_carry_adder 
         generic map(
-            Nbit => 18;
+            Nbit => 18
         )
         port map(
-            a => l1_s2_in1;
-            b => l1_s2_in2;
-            cin => '0';
-            s => l1_s2_out;
+            a => l1_s2_in1,
+            b => l1_s2_in2,
+            cin => '0',
+            s => l1_s2_out,
             cout => open
         );
     l1_r2: DFF_N
@@ -125,21 +125,21 @@ begin
             N => 18
         )
         port map(
-            clk => clk;
-            a_rst_n => rst;
-            d => l1_s2_out;
-            q => l2_s1_in2;
+            clk => clk,
+            a_rst_n => rst,
+            d => l1_s2_out,
+            q => l2_s1_in2
         );
 
     l1_s3: Ripple_carry_adder 
         generic map(
-            Nbit => 18;
+            Nbit => 18
         )
         port map(
-            a => l1_s3_in1;
-            b => l1_s3_in2;
-            cin => '0';
-            s => l1_s3_out;
+            a => l1_s3_in1,
+            b => l1_s3_in2,
+            cin => '0',
+            s => l1_s3_out,
             cout => open
         );
     l1_r3: DFF_N
@@ -147,21 +147,21 @@ begin
             N => 18
         )
         port map(
-            clk => clk;
-            a_rst_n => rst;
-            d => l1_s3_out;
-            q => l2_s2_in1;
+            clk => clk,
+            a_rst_n => rst,
+            d => l1_s3_out,
+            q => l2_s2_in1
         );
 
     l1_s4: Ripple_carry_adder 
         generic map(
-            Nbit => 18;
+            Nbit => 18
         )
         port map(
-            a => l1_s4_in1;
-            b => l1_s4_in2;
-            cin => '0';
-            s => l1_s4_out;
+            a => l1_s4_in1,
+            b => l1_s4_in2,
+            cin => '0',
+            s => l1_s4_out,
             cout => open
         );
     l1_r4: DFF_N
@@ -169,21 +169,21 @@ begin
             N => 18
         )
         port map(
-            clk => clk;
-            a_rst_n => rst;
-            d => l1_s4_out;
-            q => l2_s2_in2;
+            clk => clk,
+            a_rst_n => rst,
+            d => l1_s4_out,
+            q => l2_s2_in2
         );
 
     l1_s5: Ripple_carry_adder 
         generic map(
-            Nbit => 18;
+            Nbit => 18
         )
         port map(
-            a => l1_s5_in1;
-            b => l1_s5_in2;
-            cin => '0';
-            s => l1_s5_out;
+            a => l1_s5_in1,
+            b => l1_s5_in2,
+            cin => '0',
+            s => l1_s5_out,
             cout => open
         );
     l1_r5: DFF_N
@@ -191,22 +191,22 @@ begin
             N => 18
         )
         port map(
-            clk => clk;
-            a_rst_n => rst;
-            d => l1_s5_out;
-            q => l2_s3_in1;
+            clk => clk,
+            a_rst_n => rst,
+            d => l1_s5_out,
+            q => l2_s3_in1
         );
 
     -- Second layer of adders
     l2_s1: Ripple_carry_adder 
         generic map(
-            Nbit => 19;
+            Nbit => 19
         )
         port map(
-            a => l2_s1_in1;
-            b => l2_s1_in2;
-            cin => '0';
-            s => l2_s1_out;
+            a => l2_s1_in1,
+            b => l2_s1_in2,
+            cin => '0',
+            s => l2_s1_out,
             cout => open
         );
     l2_r1: DFF_N
@@ -214,21 +214,21 @@ begin
             N => 19
         )
         port map(
-            clk => clk;
-            a_rst_n => rst;
-            d => l2_s1_out;
-            q => l3_s1_in1;
+            clk => clk,
+            a_rst_n => rst,
+            d => l2_s1_out,
+            q => l3_s1_in1
         );
 
     l2_s2: Ripple_carry_adder 
         generic map(
-            Nbit => 19;
+            Nbit => 19
         )
         port map(
-            a => l2_s2_in1;
-            b => l2_s2_in2;
-            cin => '0';
-            s => l2_s2_out;
+            a => l2_s2_in1,
+            b => l2_s2_in2,
+            cin => '0',
+            s => l2_s2_out,
             cout => open
         );
     l2_r2: DFF_N
@@ -236,21 +236,21 @@ begin
             N => 19
         )
         port map(
-            clk => clk;
-            a_rst_n => rst;
-            d => l2_s2_out;
-            q => l3_s1_in2;
+            clk => clk,
+            a_rst_n => rst,
+            d => l2_s2_out,
+            q => l3_s1_in2
         );
 
     l2_s3: Ripple_carry_adder 
         generic map(
-            Nbit => 19;
+            Nbit => 19
         )
         port map(
-            a => l2_s3_in1;
-            b => resized_b;
-            cin => '0';
-            s => l2_s3_out;
+            a => l2_s3_in1,
+            b => resized_b,
+            cin => '0',
+            s => l2_s3_out,
             cout => open
         );
     l2_r3: DFF_N
@@ -258,22 +258,22 @@ begin
             N => 19
         )
         port map(
-            clk => clk;
-            a_rst_n => rst;
-            d => l2_s3_out;
-            q => l4_s1_in2;
+            clk => clk,
+            a_rst_n => rst,
+            d => l2_s3_out,
+            q => l4_s1_in2
         );
 
     -- Third layer of adders
     l3_s1: Ripple_carry_adder 
         generic map(
-            Nbit => 20;
+            Nbit => 20
         )
         port map(
-            a => l3_s1_in1;
-            b => l3_s1_in2;
-            cin => '0';
-            s => l3_s1_out;
+            a => l3_s1_in1,
+            b => l3_s1_in2,
+            cin => '0',
+            s => l3_s1_out,
             cout => open
         );
     l3_r1: DFF_N
@@ -281,22 +281,22 @@ begin
             N => 20
         )
         port map(
-            clk => clk;
-            a_rst_n => rst;
-            d => l3_s1_out;
-            q => l4_s1_in1;
+            clk => clk,
+            a_rst_n => rst,
+            d => l3_s1_out,
+            q => l4_s1_in1
         );
 
     -- Fourth layer of adders
     l4_s1: Ripple_carry_adder 
         generic map(
-            Nbit => 21;
+            Nbit => 21
         )
         port map(
-            a => l4_s1_in1;
-            b => l4_s1_in2;
-            cin => '0';
-            s => sum;
+            a => l4_s1_in1,
+            b => l4_s1_in2,
+            cin => '0',
+            s => sum,
             cout => open
         );
 
